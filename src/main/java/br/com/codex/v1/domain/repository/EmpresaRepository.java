3a82a1fe707c6747c66ac2e589443d2e093382f7
alyesson <54005257+alyesson@users.noreply.github.com>
2025-05-07 17:47:32 -0300

    Método FindByCnpj Classe EmpresaRepository
    Criação do método FindByCnpj na classe EmpresaRepository

--- src/main/java/br/com/codex/v1/domain/repository/EmpresaRepository.java
+++ src/main/java/br/com/codex/v1/domain/repository/EmpresaRepository.java
@@ -3,5 +3,8 @@ package br.com.codex.v1.domain.repository;
 import br.com.codex.v1.domain.cadastros.Empresa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+    Optional<Empresa> findByCnpj(String cnpj);
 }
