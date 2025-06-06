8ec74e6cf23ad3b7e4cd4a4918acf19e2e8356a5
alyesson <54005257+alyesson@users.noreply.github.com>
2025-06-06 17:25:34 -0300

    Ajuste Coluna CodigoAmbiente
    Ajuste na coluna código ambiente da tabela AmbienteNotaFiscal.

--- src/main/java/br/com/codex/v1/domain/cadastros/AmbienteNotaFiscal.java
+++ src/main/java/br/com/codex/v1/domain/cadastros/AmbienteNotaFiscal.java
@@ -4,10 +4,7 @@ import br.com.codex.v1.domain.dto.AmbienteNotaFiscalDto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
@@ -22,6 +19,7 @@ public class AmbienteNotaFiscal implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(length = 30, nullable = false)
     private Integer codigoAmbiente;
 
     public AmbienteNotaFiscal() {
