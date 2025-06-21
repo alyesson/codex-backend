ca844c84509677e81b3631b8791abfd16643bfe5
alyesson <54005257+alyesson@users.noreply.github.com>
2025-06-20 22:23:33 -0300

    Ajuste Método RemoverCaracteresEspeciais
    Ajuste de método que valida de a informação é nula ou não.

--- src/main/java/br/com/codex/v1/utilitario/RemoveCaracteresEspeciais.java
+++ src/main/java/br/com/codex/v1/utilitario/RemoveCaracteresEspeciais.java
@@ -2,6 +2,6 @@ package br.com.codex.v1.utilitario;
 
 public class RemoveCaracteresEspeciais {
     public static String removerCaracteresEspeciais(String origem) {
-        return origem.replaceAll("[^0-9]", ""); // Remove tudo exceto números
+        return origem == null ? "" : origem.replaceAll("[^0-9]", ""); // Remove tudo exceto números
     }
 }
