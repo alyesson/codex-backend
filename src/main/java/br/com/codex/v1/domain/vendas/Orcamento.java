7df6f135071d277bf0a7bfd8c96d5dd582459718
alyesson <54005257+alyesson@users.noreply.github.com>
2025-08-10 17:09:14 -0300

    Criação Atributos Orçamentos
    Criação de atributos orçamentos

--- src/main/java/br/com/codex/v1/domain/vendas/Orcamento.java
+++ src/main/java/br/com/codex/v1/domain/vendas/Orcamento.java
@@ -1,4 +1,47 @@
 package br.com.codex.v1.domain.vendas;
 
-public class Orcamento {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+public class Orcamento implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 6)
+    private String codigo;
+
+    @Column(nullable = false, length = 11)
+    private LocalDate dataEmissao;
+
+    @Column(nullable = false, length = 11)
+    private LocalDate dataValidade;
+
+    @Column(nullable = false, length = 30)
+    private String vendedor;
+
+    @Column(nullable = false, length = 6)
+    private BigDecimal valorTotal = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    private BigDecimal descontoTotal = BigDecimal.ZERO;
+
+    @Column(nullable = false)
+    private BigDecimal valorFinal = BigDecimal.ZERO;
+
+    @Column(length = 500)
+    private String observacoes;
+
 }
