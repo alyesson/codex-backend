027eebcd510d1c5b296fafafd232a3fa06b38d45
alyesson <54005257+alyesson@users.noreply.github.com>
2025-08-22 17:14:27 -0300

    Adicionar geração de relatório PDF de orçamento e templates
    Introduz a geração de relatórios em PDF para orçamento, incluindo novos endpoints, métodos de serviço e modelos JasperReports. Atualiza os modelos de relatórios de venda para consistência de layout e melhora as mensagens de erro para a geração de PDF.

--- src/main/java/br/com/codex/v1/resources/RelatorioResource.java
+++ src/main/java/br/com/codex/v1/resources/RelatorioResource.java
@@ -36,7 +36,26 @@ public class RelatorioResource {
             return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(("Erro ao gerar relatório: " + e.getMessage()).getBytes(StandardCharsets.UTF_8));
+                    .body(("Erro ao gerar Pdf da venda: " + e.getMessage()).getBytes(StandardCharsets.UTF_8));
+        }
+    }
+
+    @PreAuthorize("hasAnyRole('ADMINISTRADOR', 'SOCIO', 'GERENTE_VENDAS', 'VENDAS')")
+    @GetMapping("/orcamento/{id}")
+    public ResponseEntity<byte[]> gerarRelatorioOrcamento(@PathVariable Long id) {
+        try {
+            // Agora passa apenas o ID
+            byte[] pdfBytes = jasperReportService.generateOrcamentoReport(id);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);
+            headers.setContentDispositionFormData("filename", "venda_" + id + ".pdf");
+            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(("Erro ao gerar Pdf do orçamento: " + e.getMessage()).getBytes(StandardCharsets.UTF_8));
         }
     }
 }
--- src/main/java/br/com/codex/v1/service/JasperReportService.java
+++ src/main/java/br/com/codex/v1/service/JasperReportService.java
@@ -1,7 +1,10 @@
 package br.com.codex.v1.service;
 
 import br.com.codex.v1.domain.enums.Situacao;
+import br.com.codex.v1.domain.repository.OrcamentoItensRepository;
 import br.com.codex.v1.domain.repository.VendaItensRepository;
+import br.com.codex.v1.domain.vendas.Orcamento;
+import br.com.codex.v1.domain.vendas.OrcamentoItens;
 import br.com.codex.v1.domain.vendas.Venda;
 import br.com.codex.v1.domain.vendas.VendaItens;
 import net.sf.jasperreports.engine.*;
@@ -26,6 +29,12 @@ public class JasperReportService {
 
     @Autowired
     private VendaItensRepository vendaItensRepository;
+
+    @Autowired
+    private OrcamentoService orcamentoService;
+
+    @Autowired
+    private OrcamentoItensRepository orcamentoItensRepository;
 
     public byte[] generateVendaReport(Long vendaId) throws Exception {
         Connection connection = null;
@@ -66,7 +75,54 @@ public class JasperReportService {
             return JasperExportManager.exportReportToPdf(jasperPrint);
 
         } catch (Exception e) {
-            throw new RuntimeException("Erro ao gerar relatório: " + e.getMessage(), e);
+            throw new RuntimeException("Erro ao gerar Pdf: " + e.getMessage(), e);
+        } finally {
+            if (connection != null) {
+                try { connection.close(); } catch (SQLException e) { /* log error */ }
+            }
+        }
+    }
+
+    public byte[] generateOrcamentoReport(Long orcamentoId) throws Exception {
+        Connection connection = null;
+        try {
+
+            connection = dataSource.getConnection();
+
+            Orcamento orcamento = orcamentoService.findById(orcamentoId);
+            if (orcamento == null) {
+                throw new RuntimeException("orçamento não encontrado com ID: " + orcamentoId);
+            }
+            // Busca os Itens pelo VendaItemRepository
+            List<OrcamentoItens> itens = orcamentoItensRepository.findByOrcamentoId(orcamentoId);
+
+            Map<String, Object> parameters = new HashMap<>();
+
+            // PARÂMETRO PARA O RELATÓRIO PRINCIPAL
+            parameters.put("P_CODIGO", orcamentoId);
+            parameters.put("P_CODIGOID", orcamentoId);
+
+            parameters.put("codigo", orcamento.getCodigo());
+            parameters.put("data_emissao", orcamento.getDataEmissao());
+            parameters.put("vendedor", orcamento.getVendedor());
+            parameters.put("cliente", orcamento.getConsumidor());
+            parameters.put("documento", orcamento.getDocumentoConsumidor());
+            parameters.put("valor_total", orcamento.getValorFinal());
+            parameters.put("situacao", orcamento.getSituacao().getDescricao());
+            parameters.put("observacao", orcamento.getObservacoes());
+            parameters.put("tipo_venda", orcamento.getTipoOrcamento());
+            parameters.put("validade", orcamento.getDataValidade());
+            parameters.put("desconto", orcamento.getDescontoTotal());
+            parameters.put("frete", orcamento.getValorFrete());
+            parameters.put("REPORT_CONNECTION", connection);
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(new File("src/main/resources/reports/orcamento_template.jasper"));
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao gerar Pdf: " + e.getMessage(), e);
         } finally {
             if (connection != null) {
                 try { connection.close(); } catch (SQLException e) { /* log error */ }
