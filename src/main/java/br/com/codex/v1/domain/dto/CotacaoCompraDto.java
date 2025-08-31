bcefa46308872fc24a464b63fc7652176500625d
alyesson <54005257+alyesson@users.noreply.github.com>
2025-08-31 09:13:34 -0300

    Ajuste Entidade Cotação Compra
    Ajuste na entidade cotação compra, adição de novos campos e remoção de outros.

--- src/main/java/br/com/codex/v1/domain/dto/CotacaoCompraDto.java
+++ src/main/java/br/com/codex/v1/domain/dto/CotacaoCompraDto.java
@@ -1,6 +1,9 @@
 package br.com.codex.v1.domain.dto;
 
 import br.com.codex.v1.domain.compras.CotacaoCompra;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 import java.io.Serial;
 import java.io.Serializable;
@@ -8,6 +11,8 @@ import java.math.BigDecimal;
 import java.sql.Date;
 import java.util.List;
 
+@Getter
+@Setter
 public class CotacaoCompraDto implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
@@ -25,11 +30,13 @@ public class CotacaoCompraDto implements Serializable {
     protected String cnpj;
     protected String ie;
     protected String endereco;
-    protected String cep;
+    protected String prazoEntrega;
     protected String contato;
     protected BigDecimal valorCotado;
+    protected String linkCompra;
+    protected String condicoesPagamento;
+    protected String observacao;
     protected List<CotacaoItensCompraDto> itens;
-    protected String link;
 
     public CotacaoCompraDto() {
         super();
@@ -48,145 +55,11 @@ public class CotacaoCompraDto implements Serializable {
         this.cnpj = obj.getCnpj();
         this.ie = obj.getIe();
         this.endereco = obj.getEndereco();
-        this.cep = obj.getCep();
+        this.prazoEntrega = obj.getPrazoEntrega();
         this.contato = obj.getContato();
         this.valorCotado = obj.getValorCotado();
-        this.link = obj.getLink();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getNumeroSolicitacao() {
-        return numeroSolicitacao;
-    }
-
-    public void setNumeroSolicitacao(Integer numeroSolicitacao) {
-        this.numeroSolicitacao = numeroSolicitacao;
-    }
-
-    public String getSolicitante() {
-        return solicitante;
-    }
-
-    public void setSolicitante(String solicitante) {
-        this.solicitante = solicitante;
-    }
-
-    public Date getDataSolicitacao() {
-        return dataSolicitacao;
-    }
-
-    public void setDataSolicitacao(Date dataSolicitacao) {
-        this.dataSolicitacao = dataSolicitacao;
-    }
-
-    public Date getDataAbertura() {
-        return dataAbertura;
-    }
-
-    public void setDataAbertura(Date dataAbertura) {
-        this.dataAbertura = dataAbertura;
-    }
-
-    public Date getValidade() {
-        return validade;
-    }
-
-    public void setValidade(Date validade) {
-        this.validade = validade;
-    }
-
-    public String getSituacao() {
-        return situacao;
-    }
-
-    public void setSituacao(String situacao) {
-        this.situacao = situacao;
-    }
-
-    public String getComprador() {
-        return comprador;
-    }
-
-    public void setComprador(String comprador) {
-        this.comprador = comprador;
-    }
-
-    public String getFornecedor() {
-        return fornecedor;
-    }
-
-    public void setFornecedor(String fornecedor) {
-        this.fornecedor = fornecedor;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getIe() {
-        return ie;
-    }
-
-    public void setIe(String ie) {
-        this.ie = ie;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getContato() {
-        return contato;
-    }
-
-    public void setContato(String contato) {
-        this.contato = contato;
-    }
-
-    public BigDecimal getValorCotado() {
-        return valorCotado;
-    }
-
-    public void setValorCotado(BigDecimal valorCotado) {
-        this.valorCotado = valorCotado;
-    }
-
-    public List<CotacaoItensCompraDto> getItens() {
-        return itens;
-    }
-
-    public void setItens(List<CotacaoItensCompraDto> itens) {
-        this.itens = itens;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
+        this.linkCompra = obj.getLinkCompra();
+        this.condicoesPagamento = obj.getCondicoesPagamento();
+        this.observacao = obj.getObservacao();
     }
 }
