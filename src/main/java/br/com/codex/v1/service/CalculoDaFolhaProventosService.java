7ef686cd102538ecea53c9a67c62b9e19e458533
alyesson <54005257+alyesson@users.noreply.github.com>
2025-10-14 23:17:59 -0300

    Adição Eventos 2 e 5 Calculo Eventos
    Adição dos eventos 2 e 5 para cálculo da folha.

--- src/main/java/br/com/codex/v1/service/CalculoDaFolhaProventosService.java
+++ src/main/java/br/com/codex/v1/service/CalculoDaFolhaProventosService.java
@@ -22,6 +22,9 @@ public class CalculoDaFolhaProventosService {
     @Autowired
     private CadastroColaboradoresService cadastroColaboradoresService;
 
+    @Autowired
+    private FolhaMensalRepository folhaMensalRepository;
+
     Calendario calendario = new Calendario();
     Set<LocalDate> feriados = new HashSet<>();
 
@@ -43,7 +46,7 @@ public class CalculoDaFolhaProventosService {
     calculoMediaDsrSobreDecioTerceiro, calculoMediaDsrNoturnoSobreDecimoTerceiro, mediaHoraExtraSalarioMaternidade50Porcento,
     mediaHoraExtraSalarioMaternidade70Porcento, mediaHoraExtraSalarioMaternidade100Porcento, mediaValorDsrDiurnoSalarioMaternidade,
     mediaValorDsrNoturnoSalarioMaternidade, mediaAdicionalNoturnoSalarioMaternidade, mediaComissoesAno,
-    participacaoLucros, abonoSalarial, salarioBase, salarioPorHora;
+    participacaoLucros, abonoSalarial, salarioBase, salarioPorHora, faltasHorasMes;
 
     private BigDecimal obtemSalarioMinimo(){
         return tabelaImpostoRendaService.getSalarioMinimo();
@@ -57,9 +60,11 @@ public class CalculoDaFolhaProventosService {
         horaSaida = cadastroColaboradoresService.findByNumeroMatricula(numeroMatricula).getHorarioSaida();
         salarioBase = cadastroColaboradoresService.findByNumeroMatricula(numeroMatricula).getSalarioColaborador();
         valorValeTransporte = cadastroColaboradoresService.findByNumeroMatricula(numeroMatricula).getValeTransporteCusto();
+        faltasHorasMes = folhaMensalRepository.findByNumeroMatricula(numeroMatricula).getFaltasHorasMes();
 
         switch (codigoEvento) {
 
+            //Calcula Horas Normais Diurnas
             case 1 -> {
 
                 LocalTime horaIni = horaEntrada;
@@ -96,7 +101,6 @@ public class CalculoDaFolhaProventosService {
                             workingDaysDiurno++;
                         }
                     }
-                    /*------------------------------------------------------------------------------------*/
 
                     // Calcula o valor das horas diurnas e arredonda para 2 casas decimais
                     valorReferenciaHoraDiurna = new BigDecimal(hourDiurna).multiply(new BigDecimal(workingDaysDiurno)).setScale(2, RoundingMode.HALF_UP); // Arredonda para 2 casas decimais
@@ -133,7 +137,6 @@ public class CalculoDaFolhaProventosService {
                                 workingDaysDiurno++;
                             }
                         }
-                        /*------------------------------------------------------------------------------------*/
 
                         // Calcula o valor das horas diurnas e arredonda para 2 casas decimais
                         valorReferenciaHoraDiurna = new BigDecimal(hourDiurna).multiply(new BigDecimal(workingDaysDiurno)).setScale(2, RoundingMode.HALF_UP); // Arredonda para 2 casas decimais
@@ -180,8 +183,138 @@ public class CalculoDaFolhaProventosService {
                         resultado.put("descontos", BigDecimal.ZERO);
                     }
                 }
-
             }
+
+            //Calcula Adiantamento de Salário (40%)
+            case 2 -> {
+                //Calculando Adiantamento de Salário
+                BigDecimal adiantamentoSalarial = (salarioBase.multiply(new BigDecimal("40"))).divide(new BigDecimal("100"));
+                resultado.put("referencia", new BigDecimal(40));
+                resultado.put("vencimentos", adiantamentoSalarial);
+                resultado.put("descontos", BigDecimal.ZERO);
+            }
+
+            //Calculando Horas Repouso Remunerado Diurno no mês
+            case 5 -> {
+
+                LocalTime horaIniHRDiurno = horaEntrada;
+                LocalTime horaFimHRDiurno = horaSaida;
+                LocalTime hora22HRDiurno = LocalTime.parse("22:00");
+                LocalTime hora13HRDiurno = LocalTime.parse("13:00");
+
+                if (horaFimHRDiurno.isBefore(hora13HRDiurno)) {
+
+                    Duration horaNormalHRDiurno = Duration.between(horaIniHRDiurno, hora22HRDiurno); //Aqui eu pego a hora que entrou por exemplo 18:00 e calculo a diferença até às 22:00
+                    LocalTime diferencaHoraNormalHRDiurno = LocalTime.ofNanoOfDay(horaNormalHRDiurno.toNanos());// Aqui tenho a Diferenca de horas Diurnas
+
+                    int horasDiurnas = diferencaHoraNormalHRDiurno.getHour() - 1; // Aqui pego o valor das horas menos o horário do almoço
+                    int minutosDiurnos = diferencaHoraNormalHRDiurno.getMinute() / 60; // Aqui pego o valor dos minutos e transformo em centesimal
+
+                    String hourDiurnaDsr = String.valueOf(horasDiurnas) + "." + String.valueOf(minutosDiurnos);// Horas normais no dia
+
+                    int year = LocalDate.now().getYear();
+                    int month = LocalDate.now().getMonthValue();
+                    int notWorkingDaysDir = 0;
+
+                    YearMonth anoMes = YearMonth.of(year, month);
+
+                    feriados.addAll(calendario.getFeriadosFixos(year));
+                    feriados.addAll(calendario.getFeriadosMoveis(year));
+
+                    //--------Aqui é Encontrado os domingos e Feriados no Mês
+                    for (int dia = 1; dia <= anoMes.lengthOfMonth(); dia++) {
+                        LocalDate data = anoMes.atDay(dia);
+
+                        if (data.getDayOfWeek() == DayOfWeek.SUNDAY || feriados.contains(data)) {
+                            notWorkingDaysDir++;
+                        }
+                    }
+                    BigDecimal valorReferenciaDsrDiurno = new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr));
+                    BigDecimal dsrHoraDiurna = (new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr))).multiply(salarioPorHora);
+
+                    resultado.put("referencia", valorReferenciaDsrDiurno);
+                    resultado.put("vencimentos", dsrHoraDiurna);
+                    resultado.put("descontos", BigDecimal.ZERO);
+
+                } else {
+
+                    if (horaFimHRDiurno.isBefore(hora22HRDiurno)) {
+
+                        Duration horaNormalHRDiurno = Duration.between(horaIniHRDiurno, horaFimHRDiurno);// Faz a diferença da Hora que Entrou com a Hora que saiu e me dá o total de horas num dia.
+                        LocalTime diferencaHoraNormalHRDiurno = LocalTime.ofNanoOfDay(horaNormalHRDiurno.toNanos());// Aqui tenho a Diferenca de horas Diurnas
+
+                        int horasDiurnas = diferencaHoraNormalHRDiurno.getHour() - 1; // Aqui pego o valor das horas menos o horário do almoço
+                        int minutosDiurnos = diferencaHoraNormalHRDiurno.getMinute() / 60; // Aqui pego o valor dos minutos e transformo em centesimal
+
+                        String hourDiurnaDsr = String.valueOf(horasDiurnas) + "." + String.valueOf(minutosDiurnos);// Horas normais no dia
+
+                        int year = LocalDate.now().getYear();
+                        int month = LocalDate.now().getMonthValue();
+                        int notWorkingDaysDir = 0;
+
+                        YearMonth anoMes = YearMonth.of(year, month);
+
+                        feriados.addAll(calendario.getFeriadosFixos(year));
+                        feriados.addAll(calendario.getFeriadosMoveis(year));
+
+                        //----------Aqui é Encontrado os domingos e Feriados no Mês
+                        for (int dia = 1; dia <= anoMes.lengthOfMonth(); dia++) {
+                            LocalDate data = anoMes.atDay(dia);
+
+                            if (data.getDayOfWeek() == DayOfWeek.SUNDAY || feriados.contains(data)) {
+                                notWorkingDaysDir++;
+                            }
+                        }
+                        BigDecimal valorReferenciaDsrDiurno = new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr));
+                        BigDecimal dsrHoraDiurna = (new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr))).multiply(salarioPorHora);
+
+                        resultado.put("referencia", valorReferenciaDsrDiurno);
+                        resultado.put("vencimentos", dsrHoraDiurna);
+                        resultado.put("descontos", BigDecimal.ZERO);
+                    } else {
+                        Duration horaNormal = Duration.between(horaIniHRDiurno, hora22HRDiurno);// Faz a diferença da Hora que Entrou com a Hora que saiu e me dá o total de horas num dia.
+                        LocalTime diferencaHoraNormalHRDiurno = LocalTime.ofNanoOfDay(horaNormal.toNanos());// Aqui tenho a Diferenca de horas Diurnas
+
+                        int horasDiurnas = diferencaHoraNormalHRDiurno.getHour() - 1; // Aqui pego o valor das horas menos o horário do almoço
+                        int minutosDiurnos = diferencaHoraNormalHRDiurno.getMinute() / 60; // Aqui pego o valor dos minutos e transformo em centesimal
+
+                        String hourDiurnaDsr = String.valueOf(horasDiurnas) + "." + String.valueOf(minutosDiurnos);// Horas normais no dia
+
+                        int year = LocalDate.now().getYear();
+                        int month = LocalDate.now().getMonthValue();
+                        int notWorkingDaysDir = 0;
+
+                        YearMonth anoMes = YearMonth.of(year, month);
+
+                        feriados.addAll(calendario.getFeriadosFixos(year));
+                        feriados.addAll(calendario.getFeriadosMoveis(year));
+
+                        //----------Aqui é Encontrado os domingos e Feriados no Mês
+                        for (int dia = 1; dia <= anoMes.lengthOfMonth(); dia++) {
+                            LocalDate data = anoMes.atDay(dia);
+
+                            if (data.getDayOfWeek() == DayOfWeek.SUNDAY || feriados.contains(data)) {
+                                notWorkingDaysDir++;
+                            }
+                        }
+                        BigDecimal valorReferenciaDsrDiurno = new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr));
+                        BigDecimal dsrHoraDiurna = (new BigDecimal(notWorkingDaysDir).multiply(new BigDecimal(hourDiurnaDsr))).multiply(salarioPorHora);
+
+                        resultado.put("referencia", valorReferenciaDsrDiurno);
+                        resultado.put("vencimentos", dsrHoraDiurna);
+                        resultado.put("descontos", BigDecimal.ZERO);
+                    }
+                }
+            }
+
+            //Horas de Atestado Médico
+            case 8 -> {
+
+             BigDecimal horasDeFaltasAtestadoMedico = faltasHorasMes;
+                resultado.put("referencia", horasDeFaltasAtestadoMedico);
+                resultado.put("descontos", BigDecimal.ZERO);
+            }
+
             case 130 -> {
                 //Calculando Ajuda de Custo
                 resultado.put("referencia", valorValeTransporte);
